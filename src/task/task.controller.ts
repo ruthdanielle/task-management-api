86c4f2ef44e168b45cae4073501081809f6cdacf
Ruth Moreira <ruth.moreira@funcionalcorp.com.br>
2024-09-25 11:36:48 -0300

    api em nest js

---
+++ src/task/task.controller.ts
@@ -0,0 +1,35 @@
+import { Controller , Post, Body, Get, Param, Put, Delete, Query} from '@nestjs/common';
+import { FindAllParameters, TaskDto } from './task.dto';
+import { TaskService } from './task.service';
+
+@Controller('task')
+export class TaskController {
+
+    constructor(private readonly taskService: TaskService) { }
+
+    @Post()
+    create(@Body() task: TaskDto) {
+        this.taskService.create(task);
+    }
+
+    @Get('/:id')
+    findById(@Param('id') id: string): TaskDto{
+        return this.taskService.findById(id);
+    }
+
+    @Get()
+    findAll(@Query() params: FindAllParameters) : TaskDto[] {
+        return this.taskService.findAll(params);
+    }
+
+    @Put()
+    update(@Body() task: TaskDto) {
+        this.taskService.update(task);
+    }
+
+    @Delete('/:id')
+    remove(@Param('id') id: string) {
+        this.taskService.remove(id);
+    }
+    
+}
