6407a6be22a4c51ebb80181dcdc0be9a082eee7a
Ruth Moreira <ruth.moreira@funcionalcorp.com.br>
2024-09-26 10:57:26 -0300

    adiciona validação na task dto

--- src/task/task.dto.ts
+++ src/task/task.dto.ts
@@ -1,8 +1,30 @@
+import { IsDateString, IsEnum, IsOptional, IsString, IsUUID, MaxLength, MinLength } from "class-validator";
+
+export enum TaskDtoStatusEnum{
+    TO_DO = 'TO_DO',
+    IN_PROGRESS = 'IN_PROGRESS',
+    DONE = 'DONE'
+}
 export class TaskDto{
+    @IsUUID()
+    @IsOptional()
     id: string;
+
+    @IsString()
+    @MinLength(3)
+    @MaxLength(256)
     title: string;
+
+    @IsString()
+    @MinLength(5)
+    @MaxLength(512)
     description: string;
+
+    @IsEnum(TaskDtoStatusEnum)
+    @IsOptional()
     status: string;
+
+    @IsDateString()
     expirationDate: Date
 }
 
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,5 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { FindAllParameters, TaskDto } from './task.dto';
+import { FindAllParameters, TaskDto, TaskDtoStatusEnum } from './task.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TaskService {
@@ -7,6 +8,8 @@ export class TaskService {
     private tasks : TaskDto[] = [];
 
     create(task: TaskDto) {
+        task.id = uuid();
+        task.status = TaskDtoStatusEnum.IN_PROGRESS;
         this.tasks.push(task);
     }
 
